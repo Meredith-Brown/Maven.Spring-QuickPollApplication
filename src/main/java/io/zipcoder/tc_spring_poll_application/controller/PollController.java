eedfbcc5f24eed98790ef6e09082f02a684fa15a
meredith <meredithbrown302@gmail.com>
2022-04-24 20:20:47 -0400

    part 3.1.4 complete

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -3,11 +3,16 @@ package io.zipcoder.tc_spring_poll_application.controller;
 import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class PollController {
@@ -22,6 +27,19 @@ public class PollController {
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls", method=RequestMethod.POST)
+    public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
+        poll = pollRepository.save(poll);
+        URI newPollUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(newPollUri);
+        return new ResponseEntity<>(new HttpHeaders(), HttpStatus.CREATED);
     }
 
 }
